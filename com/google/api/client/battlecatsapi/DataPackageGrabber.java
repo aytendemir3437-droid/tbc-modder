e71bc2e8112dddb91aa978cc07ba356833145c00
TBCCodder <aytendemir3437@gmail.com>
2026-03-18 11:51:19 +0300

    Add DataPackageGrabber class for package constants

---
+++ com/google/api/client/battlecatsapi/DataPackageGrabber.java
@@ -0,0 +1,9 @@
+public class DataPackageGrabber {
+    public static final String PKG_JP = "jp.co.ponos.battlecats";
+    public static final String PKG_EN = "jp.co.ponos.battlecatsen";
+    public static final String PKG_KR = "jp.co.ponos.battlecatskr";
+    public static final String PKG_TW = "jp.co.ponos.battlecatstw";
+    
+    // Aktif sürümü tutacak değişken
+    private String currentTarget; 
+}
